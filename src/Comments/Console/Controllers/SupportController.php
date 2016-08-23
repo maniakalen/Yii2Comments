d244fc4414cd850d5a116a84ae7c891e5ba11efb
Peter Georgiev <pesho10@yahoo.com>
2016-08-23 14:30:56 +0200

    Fixing namespace path def

--- src/Comments/Console/Controllers/SupportController.php
+++ src/Comments/Console/Controllers/SupportController.php
@@ -2,13 +2,15 @@
 
 namespace Comments\Console\Controllers;
 
+use yii\console\Controller;
+
 /**
  * Class SupportController
  * @package Yii\Modules\Comments\Console\Controllers
  * @author Petar Ivanov
  * @version 1.0
  */
-class SupportController
+class SupportController extends Controller
 {
     public function actionAdd($table)
     {
