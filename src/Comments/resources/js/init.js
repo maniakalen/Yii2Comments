d6fd3f191d938fa7d4b88f15964f512e12a4e233
Peter Georgiev <pesho10@yahoo.com>
2016-10-20 12:49:35 +0200

    changin js

--- src/Comments/resources/js/init.js
+++ src/Comments/resources/js/init.js
@@ -4,6 +4,30 @@
 $(document).ready(function() {
     window.yii = window.yii || {};
     window.yii.comments = window.yii.comments || {"data" : [], "order" : "4"};
+    window.yii.comments.compileTemplate = function(selector) {
+        if (typeof window.templates == 'undefined') {
+            window.templates = {};
+        }
+        if (typeof window.templates[selector] == 'undefined') {
+            var source = $(selector).html();
+            window.templates[selector] = Handlebars.compile(source);
+        }
+        return window.templates[selector];
+    };
+    window.yii.comments.renderComments = function(data) {
+        if (typeof yii.comments.pageSize == 'number' && yii.comments.pageSize > 0) {
+            data = yii.comments.isOrderAsc()?data.slice(-yii.comments.pageSize):data.slice(0,yii.comments.pageSize);
+        }
+        var container = $(this).closest('div.handlebars-comments-container');
+        var template = container.data('handlebarsTemplate');
+        if (typeof template != 'undefined') {
+            $('div.comments-container', container).html(template(data));
+        }
+    };
+    window.yii.comments.isOrderAsc = function() {
+        return typeof yii.comments.order == 'undefined' || yii.comments.order == 4;
+    };
+
     $('div.handlebars-comments-container').each(function() {
         var table = $(this).data('commentsTable');
         var id = $(this).data('commentsId');
@@ -18,12 +42,12 @@ $(document).ready(function() {
         }).done(function(data) {
             if (data.length) {
                 yii.comments.data = data;
-                var template = compileTemplate('script#handlebars-comments');
+                var template = yii.comments.compileTemplate('script#handlebars-comments');
                 $(this).data('handlebarsTemplate', template);
                 var comments = $('<div/>').addClass('comments-container');
                 $(this).html(comments);
-                $(this).append(compileTemplate('script#handlebars-comments-form')([]));
-                renderComments(data);
+                $(this).append(yii.comments.compileTemplate('script#handlebars-comments-form')([]));
+                yii.comments.renderComments.bind(comments,data).call();
             }
         }.bind(this));
     });
@@ -42,38 +66,17 @@ $(document).ready(function() {
             'data' : {
                 content: $(this).parent().find('textarea').val(),
                 table_relation: table,
-                table_relation_id: id,
+                table_relation_id: id
             }
         }).done(function(data) {
-            var order_asc = typeof yii.comments.order == 'undefined' || yii.comments.order == 4;
-            if (order_asc) {
+            if (yii.comments.isOrderAsc()) {
                 yii.comments.data.push(data);
             } else {
                 yii.comments.data.unshift(data);
             }
             var items = yii.comments.data;
-            renderComments(items)
+            yii.comments.renderComments.bind(this,items).call();
         }.bind(this));
         return false;
     });
 });
-var compileTemplate = function(selector) {
-    if (typeof window.templates == 'undefined') {
-        window.templates = {};
-    }
-    if (typeof window.templates[selector] == 'undefined') {
-        var source = $(selector).html();
-        window.templates[selector] = Handlebars.compile(source);
-    }
-    return window.templates[selector];
-};
-var renderComments = function(data) {
-    if (typeof yii.comments.pageSize == 'number' && yii.comments.pageSize > 0) {
-        data = order_asc?data.slice(-yii.comments.pageSize):data.slice(0,yii.comments.pageSize);
-    }
-    var container = $(this).closest('div.handlebars-comments-container');
-    var template = container.data('handlebarsTemplate');
-    if (typeof template != 'undefined') {
-        $('div.comments-container', container).html(template(data));
-    }
-};
