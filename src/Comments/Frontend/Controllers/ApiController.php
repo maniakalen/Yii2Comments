de4ec110c4dfe937772a11f8c0170954b29afe91
Peter Georgiev <pesho10@yahoo.com>
2016-12-07 12:20:16 +0100

    version 1.0.5 - filter by the table and id

--- src/Comments/Frontend/Controllers/ApiController.php
+++ src/Comments/Frontend/Controllers/ApiController.php
@@ -12,6 +12,7 @@
 namespace Comments\Frontend\Controllers;
 
 
+use yii\base\InvalidCallException;
 use yii\data\ActiveDataProvider;
 use yii\helpers\ArrayHelper;
 use yii\rest\ActiveController;
@@ -33,6 +34,12 @@ class ApiController extends ActiveController
         return ArrayHelper::merge(parent::actions(), [
             'index' => [
                 'prepareDataProvider' => function() {
+                    $request = \Yii::$app->request;
+                    $table = $request->get('table');
+                    $id = $request->get('id');
+                    if (!$table || !$id) {
+                        throw new InvalidCallException('Configuration missing');
+                    }
                     /* @var $modelClass \yii\db\BaseActiveRecord */
                     $modelClass = $this->modelClass;
                     $userEntity = \Yii::$app->user->identity;
@@ -41,7 +48,11 @@ class ApiController extends ActiveController
                     $order = isset($model->defaultOrder)?$model->defaultOrder:null;
                     $query = $modelClass::find()
                         ->select(['author' => 'u.name', 'created_at' => $modelClass::tableName().'.created_at', 'text' => 'content'])
-                        ->leftJoin(['u' => $userEntity::tableName()], $modelClass::tableName() . '.user_id = u.id');
+                        ->leftJoin(['u' => $userEntity::tableName()], $modelClass::tableName() . '.user_id = u.id')
+                        ->where([
+                            $modelClass::tableName().'.table_relation' => $table,
+                            $modelClass::tableName().'.table_relation_id' => $id
+                        ]);
                     if ($order) {
                         $query->orderBy($order);
                     }
