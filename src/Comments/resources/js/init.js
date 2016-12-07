f1c3ff77438180f5408c216dd6a9a43b097dfe25
Peter Georgiev <pesho10@yahoo.com>
2016-12-07 12:12:50 +0100

    version 1.0.4

--- src/Comments/resources/js/init.js
+++ src/Comments/resources/js/init.js
@@ -60,82 +60,80 @@ $(document).ready(function() {
             "method": "GET",
             "url": url
         }).done(function(data) {
-            if (data.length) {
-                yii.comments.data = data;
-                var template = yii.comments.compileTemplate('script#handlebars-comments');
-                $(this).data('handlebarsTemplate', template);
-                var comments = $('<div/>').addClass('comments-container');
-                $(this).html(comments);
-                var numPages = Math.ceil(yii.comments.data.length / yii.comments.pageSize);
-                if (typeof yii.comments.pageSize == 'number' && yii.comments.pageSize > 0 && numPages > 1) {
-                    $(this).data('commentsPage', 1)
-                        .data('commentsDownDisabled', !yii.comments.isOrderAsc())
-                        .data('commentsUpDisabled', yii.comments.isOrderAsc());
+            yii.comments.data = data;
+            var template = yii.comments.compileTemplate('script#handlebars-comments');
+            $(this).data('handlebarsTemplate', template);
+            var comments = $('<div/>').addClass('comments-container');
+            $(this).html(comments);
+            var numPages = Math.ceil(yii.comments.data.length / yii.comments.pageSize);
+            if (typeof yii.comments.pageSize == 'number' && yii.comments.pageSize > 0 && numPages > 1) {
+                $(this).data('commentsPage', 1)
+                    .data('commentsDownDisabled', !yii.comments.isOrderAsc())
+                    .data('commentsUpDisabled', yii.comments.isOrderAsc());
 
-                    var scrollUp = $('<a/>')
-                        .html("<span class='glyphicon glyphicon-chevron-up' />")
-                        .addClass('btn btn-default up');
+                var scrollUp = $('<a/>')
+                    .html("<span class='glyphicon glyphicon-chevron-up' />")
+                    .addClass('btn btn-default up');
 
-                    var scrollDown = $('<a/>')
-                        .html("<span class='glyphicon glyphicon-chevron-down' />")
-                        .addClass('btn btn-default down');
+                var scrollDown = $('<a/>')
+                    .html("<span class='glyphicon glyphicon-chevron-down' />")
+                    .addClass('btn btn-default down');
 
-                    var goBack = function() {
-                        var parent = $(this).parent();
-                        if (parent.data('commentsUpDisabled')) {
-                            return false;
-                        }
-                        var page = parseInt(parent.data('commentsPage'));
-                        page -= 1;
-                        if (parent.data('commentsDownDisabled')) {
-                            parent.data('commentsDownDisabled', false);
-                            $('a', parent).removeClass('disabled');
-                        }
-                        if (page <= 1) {
-                            parent.data('commentsUpDisabled', true);
-                            $(this).addClass('disabled');
-                        }
-
-                        if (!isNaN(page)) {
-                            parent.data('commentsPage', Math.max(1,page))
-                            yii.comments.renderComments.bind(comments,yii.comments.data,page).call();
-                        }
-                    };
-                    var goForward = function() {
-                        var parent = $(this).parent();
-                        if (parent.data('commentsDownDisabled')) {
-                            return false;
-                        }
-                        var page = parseInt(parent.data('commentsPage'));
-                        page +=1;
-                        page = Math.min(page, numPages);
-                        if (parent.data('commentsUpDisabled')) {
-                            parent.data('commentsUpDisabled', false);
-                            $('a', parent).removeClass('disabled');
-                        }
-                        if (page >= numPages) {
-                            parent.data('commentsDownDisabled', true);
-                            $(this).addClass('disabled');
-                        }
-
-                        if (!isNaN(page)) {
-                            parent.data('commentsPage', page);
-                            yii.comments.renderComments.bind(comments,yii.comments.data,page).call();
-                        }
-                    };
-                    scrollUp.on('click', yii.comments.isOrderAsc()?goForward:goBack);
-                    scrollDown.on('click', yii.comments.isOrderAsc()?goBack:goForward);
-                    if (yii.comments.isOrderAsc()) {
-                        scrollDown.addClass('disabled');
-                    } else {
-                        scrollUp.addClass('disabled');
+                var goBack = function() {
+                    var parent = $(this).parent();
+                    if (parent.data('commentsUpDisabled')) {
+                        return false;
                     }
-                    comments.before(scrollUp);
-                    comments.after(scrollDown);
+                    var page = parseInt(parent.data('commentsPage'));
+                    page -= 1;
+                    if (parent.data('commentsDownDisabled')) {
+                        parent.data('commentsDownDisabled', false);
+                        $('a', parent).removeClass('disabled');
+                    }
+                    if (page <= 1) {
+                        parent.data('commentsUpDisabled', true);
+                        $(this).addClass('disabled');
+                    }
+
+                    if (!isNaN(page)) {
+                        parent.data('commentsPage', Math.max(1,page))
+                        yii.comments.renderComments.bind(comments,yii.comments.data,page).call();
+                    }
+                };
+                var goForward = function() {
+                    var parent = $(this).parent();
+                    if (parent.data('commentsDownDisabled')) {
+                        return false;
+                    }
+                    var page = parseInt(parent.data('commentsPage'));
+                    page +=1;
+                    page = Math.min(page, numPages);
+                    if (parent.data('commentsUpDisabled')) {
+                        parent.data('commentsUpDisabled', false);
+                        $('a', parent).removeClass('disabled');
+                    }
+                    if (page >= numPages) {
+                        parent.data('commentsDownDisabled', true);
+                        $(this).addClass('disabled');
+                    }
+
+                    if (!isNaN(page)) {
+                        parent.data('commentsPage', page);
+                        yii.comments.renderComments.bind(comments,yii.comments.data,page).call();
+                    }
+                };
+                scrollUp.on('click', yii.comments.isOrderAsc()?goForward:goBack);
+                scrollDown.on('click', yii.comments.isOrderAsc()?goBack:goForward);
+                if (yii.comments.isOrderAsc()) {
+                    scrollDown.addClass('disabled');
+                } else {
+                    scrollUp.addClass('disabled');
                 }
-                $(this).append(yii.comments.compileTemplate('script#handlebars-comments-form')([]));
-                yii.comments.renderComments.bind(comments,data, 1).call();
+                comments.before(scrollUp);
+                comments.after(scrollDown);
             }
+            $(this).append(yii.comments.compileTemplate('script#handlebars-comments-form')([]));
+            yii.comments.renderComments.bind(comments,data, 1).call();
         }.bind(this));
     });
     $('body').on('click', 'button#comment_save', function() {
