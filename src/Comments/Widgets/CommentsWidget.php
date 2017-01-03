224d6412dbdc5d2cfb638f8b7280770dcb92f620
Peter Georgiev <pesho10@yahoo.com>
2017-01-03 10:30:42 +0100

    Fixing

--- src/Comments/Widgets/CommentsWidget.php
+++ src/Comments/Widgets/CommentsWidget.php
@@ -13,8 +13,27 @@ class CommentsWidget extends \yii\base\Widget
 {
     public $table;
     public $object_id;
+
+    protected static $content;
+
+    public static function begin($config = [])
+    {
+        ob_start();
+        return parent::begin($config);
+    }
+
+    public static function end()
+    {
+        self::$content = ob_end_clean();
+        return parent::end();
+    }
+
     public function run()
     {
-        return $this->render('@Comments/resources/views/comments',['table' => $this->table, 'id' => $this->object_id]);
+        return $this->render('@Comments/resources/views/comments',[
+            'table' => $this->table,
+            'id' => $this->object_id,
+            'content' => self::$content
+        ]);
     }
 }
